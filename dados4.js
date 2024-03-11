e1bad7054cbb96ff844b249fc10f5b5a389388d8
ClaudineiMacedo <158119994+ClaudineiMacedo@users.noreply.github.com>
2024-03-10 21:04:34 -0300

    Add files via upload

---
+++ dados4.js
@@ -0,0 +1,9 @@
+const a = 3; // transforma a varialvel em uma constante.
+
+// ...
+
+// a = a + 10;
+
+// ...
+
+console.log(a);
---
+++ desafio.troca.js
@@ -0,0 +1,10 @@
+// Trocar o valor das variáveis.
+let a = 31;
+let b = 50;
+
+
+let c = a;
+a = b;
+b = c;
+console.log(a);
+console.log(b);
